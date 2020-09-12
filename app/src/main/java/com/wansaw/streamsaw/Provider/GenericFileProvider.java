ffaff1f30bcea917ac6e6885a916b5d4aa986db7
Amjid <amjiddader@gmail.com>
2020-09-11 23:56:41 -0700

    2

---
+++ app/src/main/java/com/wansaw/streamsaw/Provider/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.wansaw.streamsaw.Provider;
+
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+
+}
---
+++ app/src/main/java/com/wansaw/streamsaw/event/CastSessionStartedEvent.java
@@ -0,0 +1,13 @@
+package com.wansaw.streamsaw.event;
+
+/**
+ * Created by Tamim on 28/09/2019.
+
+ */
+
+public class CastSessionStartedEvent {
+
+    public CastSessionStartedEvent() {
+
+    }
+}
